cdd4111abb63d7fad9f351c04875cb784e8d9648
Kyle Bridburg <kbridbur@mit.edu>
2018-04-12 20:40:52 -0400

    Flips between upload screen and default

--- fileupload.js
+++ fileupload.js
@@ -7,14 +7,25 @@ function handleDrop(e){
     dom.upload.style.display="block";
 }
 
+function saveFile(e){
+    e.preventDefault();
+    e.stopPropagation();
+    dom.default.style.display="block";
+    dom.upload.style.display="none";
+}
+
 Util.events(document, {
     "DOMContentLoaded": function() {
         console.log("working");
         dom.fileUpload = Util.one("#file-drag-drop");
-        dom.fileUpload.addEventListener("dragover", function(event){event.preventDefault();}, false);
-        dom.fileUpload.addEventListener("drop", function(event){handleDrop(event);}, false);
+
 
         dom.default = Util.one("#default");
         dom.upload = Util.one("#upload");
+        dom.submit = Util.one("#submit-file");
+
+        dom.fileUpload.addEventListener("dragover", function(event){event.preventDefault();}, false);
+        dom.fileUpload.addEventListener("drop", function(event){handleDrop(event);}, false);
+        dom.submit.addEventListener("click", function(event){saveFile(event);}, false);
     }
 });
