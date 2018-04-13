866c7ae1d99ee6884bff0fd0f6bbd642c9228535
Kyle Bridburg <kbridbur@mit.edu>
2018-04-12 20:28:38 -0400

    Remove normal center panel on drop

--- fileupload.js
+++ fileupload.js
@@ -2,7 +2,9 @@ dom = {}
 
 function handleDrop(e){
     e.preventDefault();
-    console.log("hi");
+    e.stopPropagation();
+    dom.default.style.display="none";
+    dom.upload.style.display="block";
 }
 
 Util.events(document, {
@@ -11,5 +13,8 @@ Util.events(document, {
         dom.fileUpload = Util.one("#file-drag-drop");
         dom.fileUpload.addEventListener("dragover", function(event){event.preventDefault();}, false);
         dom.fileUpload.addEventListener("drop", function(event){handleDrop(event);}, false);
+
+        dom.default = Util.one("#default");
+        dom.upload = Util.one("#upload");
     }
 });
