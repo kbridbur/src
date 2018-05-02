65312166b6d4ed042ff71077b8fddb9debc89d59
Santiago Munoz <smunoz@mit.edu>
2018-05-02 18:32:15 -0400

    add sorting by titles, broken date sorting

--- titlesToggle.js
+++ titlesToggle.js
@@ -1,33 +1,131 @@
 
 Util.events(document, {
     "DOMContentLoaded": function() {
-        var fileTitle   = document.getElementsById("file-title");
-        var dateTitle   = document.getElementsById("date-title");
-        var uploadTitle = document.getElementsById("upload-title");
-
+        var fileTitle    = document.getElementById("file-title");
+        var dateTitle    = document.getElementById("date-title");
+        var uploadTitle  = document.getElementById("upload-title");
+        var allTitles    = [fileTitle, dateTitle, uploadTitle];
+        var resultsFiles = document.getElementById("results-files");
         
 
-        fileTitle.addEventListener("click", function() {
-        	this.classList.add("using");
-        	dateTitle.classList.remove("using");
-        	uploadTitle.classList.remove("using");
-        	// add function here to filter by this depending on a state , down or up
-        });
+       	var active = null;
+       	var down = '\2228';
+       	var up   = '\2227';
+       	var afterDisplay = down;
 
+        function sortDates(filesHolder, direction) {
+            filesHolder.sort(function(a, b){
+                //update this
+                bDateString = b.children[1].textContent;
+                aDateString = a.children[1].textContent;
+
+                bDateParts = bDateString.split("/");
+                aDateParts = aDateString.split("/");
+
+                console.log(aDateParts);
+
+                aDate = Date(parseInt(aDateParts[2]), parseInt(aDateParts[0]-1), parseInt(aDateParts[1]));
+                bDate = Date(parseInt(bDateParts[2]), parseInt(bDateParts[0]-1), parseInt(bDateParts[1]));
+
+                return bDate - aDate;
+               
+            });
+            if (direction == down) {
+                return filesHolder;
+            } else {
+                return filesHolder.reverse()
+            }
+        }
+
+        function sortNames(filesHolder, direction) {
+            filesHolder.sort(function(a, b){
+                return a.children[0].textContent.localeCompare(b.children[0].textContent);
+            });
+            if (direction == down) {
+                return filesHolder;
+            } else {
+                return filesHolder.reverse();
+            }
+        }
+
+        function sortUploaded(filesHolder, direction) {
+            filesHolder.sort(function(a, b){
+                return a.children[2].textContent.localeCompare(b.children[2].textContent);
+            });
+            if (direction == down) {
+                return filesHolder;
+            } else {
+                return filesHolder.reverse()
+            }
+        }
+
+        function filterByMe(o, direction) {
+            //from http://jsfiddle.net/jmarikle/vxju87nv/
+
+            var filesHolder  = Array.prototype.slice.call(resultsFiles.children); 
+
+            if (o.id[0] == 'f') {
+                filesHolder = sortNames(filesHolder, direction);
+            } else if (o.id[0] == 'd') {
+                filesHolder = sortDates(filesHolder, direction);
+            } else {
+                filesHolder = sortUploaded(filesHolder, direction);
+            }
+
+            for(var i = 0, l = filesHolder.length; i < l; i++) {
+                // store the parent node so we can reatach the item
+                var parent = filesHolder[i].parentNode;
+                // detach it from wherever it is in the DOM
+                var detatchedItem = parent.removeChild(filesHolder[i]);
+                // reatach it.  This works because we are itterating
+                // over the items in the same order as they were re-
+                // turned from being sorted.
+                parent.appendChild(detatchedItem);
+            }
+        }
+
+        function flipDirection(direction) {
+        	var newD;
+        	if (direction == down) {
+        		newD = up;
+        	} else {
+        		newD = down;
+        	}
+        	return newD;
+        }
+
+        function removeDirection() {
+        	dateTitle.classList.remove("down");
+	        fileTitle.classList.remove("down");
+	        uploadTitle.classList.remove("down");
+	        dateTitle.classList.remove("up");
+	        fileTitle.classList.remove("up");
+	        uploadTitle.classList.remove("up");
+        }
+
+        function addDirection(o, direction) {
+        	if (direction == down) {
+        		o.classList.add("down");
+        	} else {
+        		o.classList.add("up");
+        	}
+        }
 		
-		dateTitle.addEventListener("click", function() {
-        	this.classList.add("using");
-        	fileTitle.classList.remove("using");
-        	uploadTitle.classList.remove("using");
-        	// add function here to filter by this depending on a state , down or up
-        });
-
-        uploadTitle.addEventListener("click", function() {
-        	this.classList.add("using");
-        	dateTitle.classList.remove("using");
-        	fileTitle.classList.remove("using");
-        	// add function here to filter by this depending on a state , down or up
-        });
-
+		for (var i = 0; i < allTitles.length; i++) {
+			allTitles[i].addEventListener("click", function() {
+				removeDirection();
+				if (active == this) {
+	        		//filter by flipped direction
+	        		afterDisplay = flipDirection(afterDisplay);
+	        		addDirection(this, afterDisplay);
+	        		filterByMe(this, afterDisplay);
+	        	} else {
+	        		active = this;
+	        		//filter by default direction
+	        		addDirection(this, down);
+	        		filterByMe(this, down);
+	        	}
+	        });
 	    }
+	}
 });
