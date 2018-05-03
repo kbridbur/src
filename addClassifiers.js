5d527386ddede32c119e8b0de86dd9b9fda8bae3
Rebecca Weinberger <rew@mit.edu>
2018-05-02 23:04:44 -0400

    Implement typing classifiers on file upload page

---
+++ addClassifiers.js
@@ -0,0 +1,48 @@
+dom = {}
+
+Util.events(document, {
+    "DOMContentLoaded": function() {
+        dom.tags = Util.one("#tags-form-box");
+        dom.projects = Util.one("#projects-form-box");
+        dom.groups = Util.one("#groups-form-box");
+
+        dom.classifiersInputs = Util.all(".form-classifier-input");
+
+        for (var i = 0; i < dom.classifiersInputs.length; i++) {
+            var cInput = dom.classifiersInputs[i];
+
+            cInput.addEventListener('keydown', function(e) {
+                if (e.key == "Enter") {
+                    cText = this.value;
+
+                    if (!cText.trim().length == 0) {
+                        var newC = document.createElement("div");
+                        newC.classList.add("added-form-classifier");
+                        newC.innerHTML = cText;
+
+                        var closeSpan = document.createElement("span");
+                        closeSpan.classList.add("form-classifier-delete")
+                        closeSpan.innerHTML = " x";
+                        closeSpan.addEventListener("click", deleteClassifier);
+                        newC.appendChild(closeSpan);
+
+                        var classifier = this.id.substring(0, this.id.length - 11);
+                        console.log(classifier);
+                        var box = dom[classifier];
+
+                        box.appendChild(newC);
+                        this.value = "";
+                        this.focus();
+                    }
+                }
+            });
+        }
+
+
+    }
+});
+
+function deleteClassifier() {
+    var toDelete = this.parentElement;
+    toDelete.parentNode.removeChild(toDelete);
+}
