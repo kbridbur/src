86b19a4c1185c9f6f8b4e99e430e9b7cf13e38da
Santiago Munoz <smunoz@mit.edu>
2018-05-03 21:51:07 -0400

    add classifier filtering

--- scripts/main.js
+++ scripts/main.js
@@ -49,6 +49,9 @@ Util.events(document, {
 
         // titlesToggle.js
         addSortListeners();
+
+        // classifierSearch.js
+        addClassifierSearchListeners();
     }
 });
 
@@ -77,4 +80,13 @@ function populateDom() {
     dom.fileTitle           = Util.one("#file-title");
     dom.dateTitle           = Util.one("#date-title");
     dom.uploadTitle         = Util.one("#upload-title");
+
+    dom.tagPanel            = Util.one("#tags-panel");
+    dom.projectPanel        = Util.one("#projects-panel");
+    dom.groupPanel          = Util.one("#groups-panel");
+    dom.classifierSearchers = Util.all(".classifier-search");
+
+    dom.tagPanelClassifiers     = Array.prototype.slice.call(dom.tagPanel.children);
+    dom.projectPanelClassifiers = Array.prototype.slice.call(dom.projectPanel.children);
+    dom.groupPanelClassifiers   = Array.prototype.slice.call(dom.groupPanel.children);
 }
